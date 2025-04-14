e5ea156b3b9f200f8d5f0983462569f1a70daf74
Feven-Zeray <fevenzeray2028@gmail.com>
2025-04-14 01:56:50 -0500

    final

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+
+import Header from './Components/Header/Header'
+import "./commonResource/css/bootstrap.css";
+import "./commonResource/css/styles.css";
+import "./commonResource/js/bootstrap"
+import AlertSection from './Components/AlertSection/AlertSection'
+import SectionOne from './Components/SectionOne/SectionOne'
+import SectionTwo from './Components/SectionTwo/SectionTwo'
+import SectionThree from './Components/SectionThree/SectionThree'
+import SectionFour from './Components/SectionFour/SectionFour'
+import SectionFive from './Components/SectionFive/SectionFive'
+import SectionSix from './commonResource copy/SectionSix/SectionSix'
+import Footer from "./Components/Footer/Footer";
+function App() {
+  
+
+  return (
+    <>
+    
+       <Header/>
+       <AlertSection/>
+        <SectionOne/>
+        <SectionTwo/>
+        <SectionThree/>
+        <SectionFour/>
+        <SectionFive/>
+        <SectionSix/>
+        
+       <Footer/>
+    </>
+  )
+}
+
+export default App
