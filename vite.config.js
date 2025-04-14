b1c0ba26af61bdb5dd3cb1e5e9df8fa77f544dd5
Feven-Zeray <fevenzeray2028@gmail.com>
2025-04-14 03:05:17 -0500

    viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+
 export default defineConfig({
   base: "/Apple_React/",
   plugins: [react()],
